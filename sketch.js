4e984103da6054bb117251558474aa68420dbb15
silverroxy79 <vabro001@mail.goucher.edu>
2016-07-23 09:16:47 -0400

    Initial Commit
    Day1-Practice

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var x = -24
+var y = -24
+
+function setup() {
+  createCanvas(200,200)
+  frameRate(25)
+}
+
+function draw() {
+  background(51,102,255)
+   fill(0,153,0)
+   stroke(255)
+  rect(75,75,25,25)
+   fill(128,0,64)
+   noStroke()
+  ellipse(125,125,50,50)
+   fill(100,75)
+  ellipse(100,100,100,100)
+   fill(255)
+   noStroke()
+  ellipse(x,y,25,25)
+  
+  x = x + 1
+  y = y + 1
+  
+  if (x > 35 && y > 35) {
+    fill(255,0,0)
+  }
+  
+  }
